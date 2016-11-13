e6dcf0d991969581bf1627d6cc765862175db2bb
Dovi Salomon <Dovisal1@gmail.com>
2016-11-13 13:00:53 -0500

    Added shebang to the main script

--- citi.sh
+++ citi.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
 
-rm -f loc.txt final.txt *zip *csv
-touch loc.txt
+rm -f loc.txt res
 
 for i in `seq 9`; do
 	f="20160$i-citibike-tripdata"
-	wget http://witestlab.poly.edu/bikes/$f.zip
-	unzip $f.zip
+	if ! test -f $f.csv; then
+		if ! test -f $f.zip; then
+			wget http://witestlab.poly.edu/bikes/$f.zip
+		fi
+		unzip $f.zip
+	fi
 	awk -F "\"*,\"*" '{print $5}' $f.csv >> loc.txt
 	awk -F "\"*,\"*" '{print $9}' $f.csv >> loc.txt
 done
 
-cat loc.txt | sort | uniq -c | sort -nr | head -n1
+cat loc.txt | sort | uniq -c | sort -nr | head -n1 > res
 rm loc.txt *zip *csv
